02ad7b1be89003c1041a9bb59349a992098ab3ec
ManikanteshwaraReddy <22bd1a0529@gmail.com>
2026-04-07 13:51:02 +0530

    build path fix

--- frontend/src/settings/axiosInstance.js
+++ frontend/src/settings/axiosInstance.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
 axios.defaults.withCredentials = true; 
-// Create an instance of Axios with custom configuration
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL, // Use environment variable for backend server URL
-    timeout: 10000, // Request timeout in milliseconds
+    baseURL: process.env.REACT_APP_BACKEND_URL, 
+    timeout: 10000, 
     headers: {
-        'Content-Type': 'application/json', // Default content type
-        // Add any other headers as needed
+        'Content-Type': 'application/json', 
     },
 });
 
